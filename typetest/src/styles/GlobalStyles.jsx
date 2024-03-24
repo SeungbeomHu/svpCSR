4830bfacf2c692a7b99ab712b0726c6c6027e7da
김장호 <rlawkdgh0406@naver.com>
2024-03-24 14:02:44 +0900

    feat: 전역 스타일 추가

---
+++ typetest/src/styles/GlobalStyles.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  height: 100vh;
+  width: 100%;
+  max-width: 500px;
+  background-color: #f0f0f0;
+
+  @media (max-width: 500px) {
+    max-width: 100%;
+    height: auto;
+  }
+`
+
+export const Button = styled.button`
+  width: 80%;
+  border-radius: 10px; /* 모서리의 둥근 정도 */
+  border: 2px solid transparent; /* 초기 테두리 설정 */
+  background-color: #4164fa; /* 기본 배경색 */
+  color: white; /* 글자색 */
+  font-size: 20px; /* 글자 크기 */
+  font-weight: bold; /* 글자 두께 */
+  cursor: pointer; /* 마우스 오버 시 커서 모양 변경 */
+  transition: all 0.3s ease-in-out; /* 상태 변화 애니메이션 */
+
+  &:hover {
+    background-color: #3399ff; /* 호버 시 배경색 변경 */
+  }
+`
