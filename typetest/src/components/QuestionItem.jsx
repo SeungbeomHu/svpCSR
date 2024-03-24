45350283dae888543fc0e142a04fff584b389ca3
김장호 <rlawkdgh0406@naver.com>
2024-03-24 15:02:03 +0900

    feat: 질문 테스트 기본 구조 구현 완료

--- typetest/src/components/QuestionItem.jsx
+++ typetest/src/components/QuestionItem.jsx
@@ -1,9 +1,15 @@
 import React from "react"
 
-const QuestionItem = () => {
+const QuestionItem = ({ data, onAnswer }) => {
+  const question = data.question
+  const firstOption = data.answers[0]
+  const secondOption = data.answers[1]
+
   return (
     <div>
-      <h1>질문 리스트</h1>
+      <p>{question}</p>
+      <button onClick={onAnswer}>{firstOption.text}</button>
+      <button onClick={onAnswer}>{secondOption.text}</button>
     </div>
   )
 }
--- typetest/src/pages/QuestionPage.jsx
+++ typetest/src/pages/QuestionPage.jsx
@@ -1,15 +1,33 @@
-import React from "react"
+import React, { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import QuestionItem from "../components/QuestionItem"
 import questions from "../data/questions"
 
 const QuestionPage = () => {
-  // 질문 및 답변 데이터
-  const questionList = questions
+  const navigate = useNavigate()
 
-  console.log(questionList)
+  // 질문 번호
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
+
+  // 다음 질문으로 넘어가는 함수
+  const handleAnswer = () => {
+    if (currentQuestionIndex < questions.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1)
+    } else {
+      // 테스트 완료
+      navigate("/result")
+    }
+  }
 
   return (
     <div>
-      <h1>질문 리스트</h1>
+      {questions.length > 0 && currentQuestionIndex < questions.length && (
+        <QuestionItem
+          key={currentQuestionIndex}
+          data={questions[currentQuestionIndex]} // 현재 질문
+          onAnswer={handleAnswer}
+        />
+      )}
     </div>
   )
 }
